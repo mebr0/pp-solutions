a0afb79e4934b3330d84e4c6dd7a4450c14c1ae1
MeBr0 <mebr0mebr1@gmail.com>
2021-11-30 18:38:40 +0600

    solve week12 problem f

--- week12/f.cpp
+++ week12/f.cpp
@@ -15,25 +15,24 @@ double compute_gpa(pair<int, int> pts) {
 
     int att = pts.first + pts.second;
 
-    // todo: what is wrong with values?
     if (95 <= att) {
         return 4;
     } else if (90 <= att) {
-        return 3.66666925;
+        return 11.0 / 3;
     } else if (85 <= att) {
-        return 3.33333425;
+        return 10.0 / 3;
     } else if (80 <= att) {
         return 3;
     } else if (75 <= att) {
-        return 2.66666925;
+        return 8.0 / 3;
     } else if (70 <= att) {
-        return 2.33333425;
+        return 7.0 / 3;
     } else if (65 <= att) {
         return 2;
     } else if (60 <= att) {
-        return 1.66666925;
+        return 5.0 / 3;
     } else if (55 <= att) {
-        return 1.33333425;
+        return 4.0 / 3;
     } else if (50 <= att) {
         return 1;
     } else {
